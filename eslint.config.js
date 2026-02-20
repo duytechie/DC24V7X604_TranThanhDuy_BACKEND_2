86e35ed11320aefcd25866e6ab76e8529809e2c1
Tom <tom@users.noreply.github.com>
2026-02-20 07:16:53 +0700

    ajv vulnerability fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginVue.configs["flat/essential"],
-]);
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  prettier,
+];
