1afcd94123ebb8c104c850611c739290802b6228
Tom <tom@users.noreply.github.com>
2026-02-21 10:00:35 +0700

    Cai dat handler update

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -50,8 +50,22 @@ export const findOne = async (req, res, next) => {
   }
 };
 
-export const update = (req, res) => {
-  res.send({ message: "update handler" });
+export const update = async (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return next(new ApiError(400, "Data to update can not be empty"));
+  }
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.update(req.params.id, req.body);
+    if (!document) {
+      return next(new ApiError(404, "Contact not found"));
+    }
+    return res.send({ message: "Contact was updated successfully" });
+  } catch (error) {
+    return next(
+      new ApiError(500, `Error updating contact with id=${req.params.id}`),
+    );
+  }
 };
 
 export const delete_ = (req, res) => {
